8acdcb7793c584305a448d510be47fe452cf710b
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-12-11 17:14:50 +0800

    动画播放  制定帧数 和暂停

---
+++ Assets/Study/Scripts/Biker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biker : MonoBehaviour
+{
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+
+        anim.Play("Death",0,1f);
+
+        Invoke("Stop",0.1f);
+
+	}
+	
+    void Stop()
+    {
+        anim.speed = 0;
+        anim.Play("Death", 0, 0.5f);
+    }
+
+}
