ed5d090994872961ff09dbe0fdff528c622632f7
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-12-13 14:41:54 +0800

    角色动画的更改 还有 控制

--- Assets/Study/Scripts/Biker.cs
+++ Assets/Study/Scripts/Biker.cs
@@ -6,17 +6,62 @@ public class Biker : MonoBehaviour
 {
     Animator anim;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         anim = GetComponent<Animator>();
 
-        anim.Play("Death",0,1f);
+        //Test1();
 
-        Invoke("Stop",0.1f);
+    }
 
-	}
-	
-    void Stop()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Pose");
+        }
+        bool isMove = false;
+        float hor = 0, ver = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            isMove = true;
+            ver += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            isMove = true;
+            ver -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            isMove = true;
+            hor -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            isMove = true;
+            hor += 1;
+        }
+        if (isMove && (hor != 0 || ver != 0))
+        {
+            transform.Translate(new Vector3(hor, 0, ver) * Time.deltaTime);
+        }
+
+        anim.SetBool("IsMove", isMove);
+        anim.SetFloat("Horizontal", hor);
+        anim.SetFloat("Vertical", ver);
+    }
+
+
+    void Test1()
+    {
+        anim.Play("Death", 0, 1f);
+        Invoke("Test1_1", 0.1f);
+
+    }
+
+    void Test1_1()
     {
         anim.speed = 0;
         anim.Play("Death", 0, 0.5f);
