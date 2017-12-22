7585918856e14849b7c62dbb040f8439bd45461c
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-12-22 11:30:53 +0800

    行走添加

---
+++ Assets/Study/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float ver = Input.GetAxis("Vertical");
+        if (ver!=0)
+        {
+            anim.SetFloat("Speed", Mathf.Abs(ver));
+        }
+    }
+}
