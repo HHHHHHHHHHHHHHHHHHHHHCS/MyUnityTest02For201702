617df333bb5a4e023aa94bb148f2b51a3fc5e8e7
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-12-25 12:22:37 +0800

    添加角色 移动转弯

--- Assets/Study/Scripts/Player.cs
+++ Assets/Study/Scripts/Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public bool firstMoveType;
+    public bool isFirstMoveType;
 
     private Animator anim;
-    private int speedID = Animator.StringToHash("Speed"); 
-
+    private int speedID = Animator.StringToHash("Speed");
+    private int isSpeedUpID = Animator.StringToHash("IsSpeedUp");
+    private int horizontalID = Animator.StringToHash("Horizontal");
 
     private void Awake()
     {
@@ -17,17 +18,60 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        float ver = Input.GetAxis("Vertical");
-        if (ver!=0)
+        Move();
+    }
+
+    public void Move()
+    {
+        object _speedID, _isSpeedUpID, _horizontalID;
+        if (isFirstMoveType)
         {
-            if(firstMoveType)
-            {
-                anim.SetFloat("Speed", Mathf.Abs(ver));
-            }
-            else
-            {
-                anim.SetFloat(speedID, Mathf.Abs(ver));
-            }
+            _speedID = "Speed";
+            _isSpeedUpID = "IsSpeedUp";
+            _horizontalID = "Horizontal";
+        }
+        else
+        {
+            _speedID = speedID;
+            _isSpeedUpID = isSpeedUpID;
+            _horizontalID = horizontalID;
+        }
+
+
+        float ver = Input.GetAxis("Vertical");
+        SetFloat(_speedID, ver);
+
+        bool isSpeedUp = Input.GetKey(KeyCode.LeftShift);
+        Debug.Log(isSpeedUp);
+        SetBool(_isSpeedUpID, isSpeedUp);
+
+        float hor = Input.GetAxis("Horizontal");
+        SetFloat(_horizontalID, hor);
+    }
+
+
+
+    public void SetFloat(object obj, float value)
+    {
+        if (obj is int)
+        {
+            anim.SetFloat((int)obj, value);
+        }
+        else if (obj is string)
+        {
+            anim.SetFloat((string)obj, value);
+        }
+    }
+
+    public void SetBool(object obj, bool value)
+    {
+        if (obj is int)
+        {
+            anim.SetBool((int)obj, value);
+        }
+        else if (obj is string)
+        {
+            anim.SetBool((string)obj, value);
         }
     }
 }
