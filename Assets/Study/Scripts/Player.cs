3af2d07809f0c6a0d9ac585d90c82a7f16667d55
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-12-25 16:11:03 +0800

    翻墙动画    和动画匹配

--- Assets/Study/Scripts/Player.cs
+++ Assets/Study/Scripts/Player.cs
@@ -10,6 +10,9 @@ public class Player : MonoBehaviour
     private int speedID = Animator.StringToHash("Speed");
     private int isSpeedUpID = Animator.StringToHash("IsSpeedUp");
     private int horizontalID = Animator.StringToHash("Horizontal");
+    private int vaultID = Animator.StringToHash("Vault");
+
+    private Vector3 matchTarget;
 
     private void Awake()
     {
@@ -23,32 +26,84 @@ public class Player : MonoBehaviour
 
     public void Move()
     {
-        object _speedID, _isSpeedUpID, _horizontalID;
+        object _speedID, _isSpeedUpID, _horizontalID, _vaultID;
         if (isFirstMoveType)
         {
             _speedID = "Speed";
             _isSpeedUpID = "IsSpeedUp";
             _horizontalID = "Horizontal";
+            _vaultID = "Vault";
         }
         else
         {
             _speedID = speedID;
             _isSpeedUpID = isSpeedUpID;
             _horizontalID = horizontalID;
+            _vaultID = vaultID;
         }
 
 
-        float ver = Input.GetAxis("Vertical");
+        float ver = Input.GetAxis("Vertical") * 5;
         SetFloat(_speedID, ver);
 
         bool isSpeedUp = Input.GetKey(KeyCode.LeftShift);
-        Debug.Log(isSpeedUp);
         SetBool(_isSpeedUpID, isSpeedUp);
 
         float hor = Input.GetAxis("Horizontal");
         SetFloat(_horizontalID, hor);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run"))
+            {
+                if(GetFloat(_speedID)>3)
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(transform.position + Vector3.up * 0.07f
+                        , transform.forward, out hit, 4.5f))
+                    {
+                        if (hit.collider.CompareTag("Obstacle"))
+                        {
+                            if(hit.distance>3)
+                            {
+                                Vector3 hitPoint = hit.point;
+                                hitPoint.y = hit.collider.transform.position.y
+                                    + hit.collider.bounds.size.y+0.1f;
+                                matchTarget = hitPoint;
+                                SetTrigger(_vaultID);
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+        //MatchTargetWeightMask 里面的两个 float   第一个是要开始触摸的时间  第二个是 触摸点的时间  两个都是0-1
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Vault"))
+        {
+            anim.MatchTarget(matchTarget, Quaternion.identity, AvatarTarget.LeftHand, new MatchTargetWeightMask(Vector3.one, 0), 0.32f, 0.4f);
+        }
     }
 
+
+    public float GetFloat(object obj)
+    {
+        if (obj is int)
+        {
+            return anim.GetFloat((int)obj);
+        }
+        else if (obj is string)
+        {
+            return anim.GetFloat((string)obj);
+        }
+        return 0;
+    }
 
 
     public void SetFloat(object obj, float value)
@@ -72,6 +127,18 @@ public class Player : MonoBehaviour
         else if (obj is string)
         {
             anim.SetBool((string)obj, value);
+        }
+    }
+
+    public void SetTrigger(object obj)
+    {
+        if (obj is int)
+        {
+            anim.SetTrigger((int)obj);
+        }
+        else if (obj is string)
+        {
+            anim.SetTrigger((string)obj);
         }
     }
 }
